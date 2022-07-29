7683fdc3d115214072189165437d866f0289faf8
davi-souza2001 <davisouza2001dv@gmail.com>
2022-07-29 15:31:11 -0300

    page login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,27 +14,27 @@ export function Login() {
 	}
 
 	return (
-		<div className='h-screen w-screen flex items-center justify-start flex-col xl:flex-row'>
+		<div className={`h-screen w-screen flex items-center justify-start flex-col ${!loginArea && 'xl:flex-row'}`}>
 			{loginArea ?
-				<>
-					<div className='h-60 w-screen flex items-center justify-center font-semibold text-5xl'>
+				<div>
+					<div className='h-60 w-full flex items-center justify-center font-semibold text-5xl'>
 						<span>Let's you in</span>
 					</div>
-					<div className='h-60 w-screen flex items-center justify-start flex-col'>
+					<div className='h-60 w-full flex items-center justify-start flex-col'>
 						<div
 							className='bg-[#292c37] h-14 w-80 rounded-lg font-semibold cursor-pointer flex items-center justify-center mb-3'
 						>
 							<img className='h-10' src={googleIcon} alt="Google Icon" />
-							<button className='ml-3'>Continue with Google</button>
+							<button className='ml-3' onClick={() => navigate('/')}>Continue with Google</button>
 						</div>
 						<div
 							className='bg-[#292c37] h-14 w-80 rounded-lg font-semibold cursor-pointer flex items-center justify-center'
 						>
 							<img className='h-10' src={githubIcon} alt="Github Icon" />
-							<button className='ml-3'>Continue with Github</button>
+							<button className='ml-3' onClick={() => navigate('/')}>Continue with Github</button>
 						</div>
 					</div>
-				</>
+				</div>
 				:
 				<div>
 					<div className='h-80 w-screen mt-10 xl:w-1/2 '>
@@ -47,8 +47,11 @@ export function Login() {
 				</div>
 			}
 			<div className='h-36 w-screen flex items-center justify-center xl:h-screen xl:flex-col'>
-				<div className={`hidden ${!loginArea && 'xl:flex'} h-44 w-full items-center justify-center font-semibold text-3xl p-5`}>
-					<span>Your Perfect Pomodoro for Planning your activities</span>
+				<div className={`hidden ${!loginArea && 'xl:flex'} h-44 w-full items-center justify-center font-semibold text-5xl p-5`}>
+					<span>Pomodoro dev</span>
+				</div>
+				<div className={`hidden ${!loginArea && 'xl:flex'} h-44 w-full items-center justify-center font-semibold text-2xl p-5`}>
+					<span>Your time better managed by you</span>
 				</div>
 				<button
 					className='bg-[#b64448] h-14 w-80 rounded-lg font-semibold text-xl cursor-pointer'
