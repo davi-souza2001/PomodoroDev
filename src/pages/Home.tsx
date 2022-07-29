30d8e95fa37b6e918e910b4193d571aa519e1db0
davi-souza2001 <davisouza2001dv@gmail.com>
2022-07-28 22:00:00 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+	return (
+		<div>
+			<h1>Olasdfsd</h1>
+		</div>
+	)
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import backgroundLogin from '../../public/backgroundLogin.svg'
+import googleIcon from '../../public/googleIcon.svg'
+import githubIcon from '../../public/githubIcon.svg'
+
+export function Login() {
+	const navigate = useNavigate()
+	const [loginArea, setLoginArea] = useState(false)
+
+	function handleOpenLoginArea() {
+		setLoginArea(!loginArea)
+	}
+
+	return (
+		<div className='h-screen w-screen flex items-center justify-start flex-col xl:flex-row'>
+			{loginArea ?
+				<>
+					<div className='h-60 w-screen flex items-center justify-center font-semibold text-5xl'>
+						<span>Let's you in</span>
+					</div>
+					<div className='h-60 w-screen flex items-center justify-start flex-col'>
+						<div
+							className='bg-[#292c37] h-14 w-80 rounded-lg font-semibold cursor-pointer flex items-center justify-center mb-3'
+						>
+							<img className='h-10' src={googleIcon} alt="Google Icon" />
+							<button className='ml-3'>Continue with Google</button>
+						</div>
+						<div
+							className='bg-[#292c37] h-14 w-80 rounded-lg font-semibold cursor-pointer flex items-center justify-center'
+						>
+							<img className='h-10' src={githubIcon} alt="Github Icon" />
+							<button className='ml-3'>Continue with Github</button>
+						</div>
+					</div>
+				</>
+				:
+				<div>
+					<div className='h-80 w-screen mt-10 xl:w-1/2 '>
+						<img className='h-full w-screen xl:w-2/2'
+							src={backgroundLogin} alt="backgroundLogin" />
+					</div>
+					<div className='h-44 w-full flex items-center justify-center font-semibold text-3xl p-5'>
+						<span>Your Perfect Pomodoro for Planning your activities</span>
+					</div>
+				</div>
+			}
+			<div className='h-36 w-screen flex items-center justify-center xl:h-screen xl:flex-col'>
+				<div className={`hidden ${!loginArea && 'xl:flex'} h-44 w-full items-center justify-center font-semibold text-3xl p-5`}>
+					<span>Your Perfect Pomodoro for Planning your activities</span>
+				</div>
+				<button
+					className='bg-[#b64448] h-14 w-80 rounded-lg font-semibold text-xl cursor-pointer'
+					onClick={handleOpenLoginArea}
+				>
+					{loginArea ? 'Back' : 'Next'}
+				</button>
+			</div>
+		</div>
+	)
+}
