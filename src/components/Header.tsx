7ceca1cf08d5eff31c1807afe19a8541640d45ef
davi-souza2001 <davisouza2001dv@gmail.com>
2022-08-01 20:47:57 -0300

    add circular progress bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,14 +9,6 @@ export function Header() {
 					src={PomodoroLogo} alt='logo' />
 				<p className='mr-10'>Pomo</p>
 			</div>
-			<div className='h-20 w-full flex items-center justify-start font-semibold text-2xl'>
-				<span className='ml-10'>Morning, Davi 👋</span>
-			</div>
-			<div className='h-28 w-full flex items-center justify-center font-semibold text-2xl'>
-				<div className='h-full w-[90%] bg-[#23262f] p-10 rounded-lg flex items-center justify-center'>
-					<span>Morning, Davi 👋</span>
-				</div>
-			</div>
 		</div>
 	)
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
 import { Header } from "../components/Header"
+import { RingProgress, Text } from '@mantine/core'
 
 export function Home() {
 	return (
 		<div>
 			<Header />
+			<div className='h-20 w-full flex items-center justify-start font-semibold text-2xl'>
+				<span className='ml-10'>Morning, Davi 👋</span>
+			</div>
+			<div className='h-28 w-full flex items-center justify-center font-semibold text-xl'>
+				<div className='h-full w-[90%] bg-[#23262f] p-10 rounded-lg flex items-center justify-start'>
+					<RingProgress
+						label={
+							<div className='flex items-center justify-center'>
+								123
+							</div>}
+						sections={[
+							{ value: 40, color: '#c64b4f' }
+						]}
+					/>
+					<p>Wow! Look at the level you've reached 🚀</p>
+				</div>
+			</div>
 		</div>
 	)
 }
